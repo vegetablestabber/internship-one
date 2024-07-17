90b0b16e359f38e1cd57db0ded05882a1f8bf5a9
Hariidaran Tamilmaran <souper-maario-gaalaxi-wahoo@protonmail.com>
2024-07-17 18:43:43 +0800

    Rearranged constants

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,20 @@
 from pathlib import Path
 
+# Paths
 # Note: File paths are relative to the 'notebooks' folder, so this could break if used outside of the 'notebooks' folder.
+
+DATA_PATH = Path("../data")
+"""Data folder path."""
+
+EXPORTS_PATH = Path("../exports")
+"""Exports folder path."""
+
+# Similarity score options
 
 DIFF_THRESHOLD = 20
 """Standard code difference threshold, 't'."""
+
+# Natural language processing (NLP) options
 
 LLM_ID = "llama3"
 """
@@ -13,9 +24,3 @@ For more on Ollama's supported models, visit https://ollama.com/library.
 
 LLM_OPTIONS = {"temperature": 0.2, "top_k": 10, "top_p": 0.5}
 """LLM options for Ollama."""
-
-DATA_PATH = Path("../data")
-"""Data folder path."""
-
-EXPORTS_PATH = Path("../exports")
-"""Exports folder path."""
