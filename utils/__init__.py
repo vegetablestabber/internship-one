12bbd2bb1ae495d87093305853f2f911a91abd57
Hariidaran Tamilmaran <souper-maario-gaalaxi-wahoo@protonmail.com>
2024-07-17 12:37:13 +0800

    Moved correspondence data to the data folder

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,16 +5,16 @@ from .industry import IndustryStandard
 # Note: File paths are relative to the 'notebooks' folder, so this could break if used outside of the 'notebooks' folder.
 
 DATA_PATH = Path("../data")
-"""Data folder path"""
+"""Data folder path."""
 
 EXPORTS_PATH = Path("../exports")
-"""Exports folder path"""
+"""Exports folder path."""
 
 CHUAN_FU_PATH = Path("../Chuan Fu")
-"""Path to Chuan Fu's files"""
+"""Path to all of Chuan Fu's files (not included in the project)."""
 
 STANDARDS = [std for std in IndustryStandard]
-"""Collection of industry standards"""
+"""Collection of industry standards."""
 
 DIFF_THRESHOLD = 20
-"""Standard code difference threshold, 't'"""
+"""Standard code difference threshold, 't'."""
--- utils/correspondence/__init__.py
+++ utils/correspondence/__init__.py
@@ -1,4 +1,4 @@
-from utils import CHUAN_FU_PATH
+from utils import DATA_PATH
 
-CORRESPONDENCE_PATH = CHUAN_FU_PATH / "Standardized codes and corresponding tables - trimmed.xlsx"
+CORRESPONDENCE_PATH = DATA_PATH / "Standardized codes and corresponding tables - trimmed.xlsx"
 """Path of the industry classification standard correspondence spreadsheet."""
